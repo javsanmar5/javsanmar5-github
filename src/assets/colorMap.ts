a96631274945324dbe651e0dfad1c1caf76c4744
Javier Santos <javsanmar5@alum.us.es>
2025-01-17 17:44:04 +0100

    feat: Add colors table to techStack tags Closes #3

---
+++ src/assets/colorMap.ts
@@ -0,0 +1,13 @@
+
+const colorMap: Record<string, string> = {
+	"Python": "bg-yellow-600",
+	"Django": "bg-green-800",
+	"JavaScript": "bg-yellow-500",
+	"TypeScript": "bg-blue-700",
+	"HTML": "bg-red-500",
+	"Java": "bg-red-700",
+	"Astro": "bg-purple-500",
+	"Tailwind": "bg-blue-400",
+}
+
+export default colorMap;
